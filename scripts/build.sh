110e017e8bc23bb6acf2c92a6b897bd7dbdf7145
Jason Cummings <jasoncummings86@gmail.com>
2021-03-15 17:16:17 -0400

    Use forked buildpack

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
-GOOS="linux" go build -ldflags='-s -w' -o bin/main github.com/projectriff/streaming-http-adapter-buildpack/cmd/main
+# GOOS="linux" go build -ldflags='-s -w' -o bin/main github.com/heroku/riff-streaming-http-adapter-buildpack/cmd/main # uncomment me when heroku is public
+GOOS="linux" go build -ldflags='-s -w' -o bin/main github.com/jsncmgs1/streaming-http-adapter-buildpack/cmd/main # delete me when heroku is public
 
 strip bin/main
 upx -q -9 bin/main
