ab9ebd80890fb0a574c302573f5cedb71aac7c6c
Jason Cummings <jasoncummings86@gmail.com>
2021-03-16 12:48:20 -0400

    update package

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-GOOS="linux" go build -ldflags='-s -w' -o bin/main github.com/heroku/riff/streaming-http-adapter-buildpack/cmd/main
+GOOS="linux" go build -ldflags='-s -w' -o bin/main github.com/jsncmgs1/streaming-http-adapter-buildpack
 
 strip bin/main
 upx -q -9 bin/main
